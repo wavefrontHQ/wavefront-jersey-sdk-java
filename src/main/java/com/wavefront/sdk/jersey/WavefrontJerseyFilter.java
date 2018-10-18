40243aec79ea5494391f3f48034be9f418e9c62c
Hao Song <mail@songhao.xyz>
2018-10-17 22:43:23 -0700

    Remove servlet filter and redundant dependencies

--- src/main/java/com/wavefront/sdk/jersey/WavefrontJerseyFilter.java
+++ src/main/java/com/wavefront/sdk/jersey/WavefrontJerseyFilter.java
@@ -8,32 +8,25 @@ import io.opentracing.Scope;
 import io.opentracing.Span;
 import io.opentracing.SpanContext;
 import io.opentracing.Tracer;
-import io.opentracing.contrib.jaxrs2.internal.CastUtils;
-import io.opentracing.contrib.jaxrs2.server.ServerHeadersExtractTextMap;
-import io.opentracing.contrib.jaxrs2.server.ServerSpanDecorator;
+import io.opentracing.propagation.TextMap;
 import io.opentracing.tag.Tags;
 import io.opentracing.propagation.Format;
-import io.opentracing.contrib.jaxrs2.internal.SpanWrapper;
-import io.opentracing.contrib.jaxrs2.server.OperationNameProvider;
 import org.glassfish.jersey.server.ContainerRequest;
 import org.glassfish.jersey.server.ExtendedUriInfo;
 import org.glassfish.jersey.server.internal.routing.RoutingContext;
 
-import javax.servlet.*;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.annotation.Nullable;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.core.MultivaluedMap;
 import java.io.IOException;
 import java.lang.management.ManagementFactory;
 import java.lang.reflect.Method;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.List;
-import java.util.Collections;
-import java.util.Optional;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -42,36 +35,31 @@ import jersey.repackaged.com.google.common.base.Preconditions;
 
 import static com.wavefront.sdk.common.Constants.NULL_TAG_VAL;
 import static com.wavefront.sdk.common.Constants.WAVEFRONT_PROVIDED_SOURCE;
-import static io.opentracing.contrib.jaxrs2.internal.SpanWrapper.PROPERTY_NAME;
 
 /**
  * A filter to generate Wavefront metrics and histograms for Jersey API requests/responses.
  *
  * @author Sushant Dewan (sushant@wavefront.com).
  */
-public class WavefrontJerseyFilter implements ContainerRequestFilter, ContainerResponseFilter, Filter {
+public class WavefrontJerseyFilter implements ContainerRequestFilter, ContainerResponseFilter {
 
   private final SdkReporter wfJerseyReporter;
   private final ApplicationTags applicationTags;
   private final ThreadLocal<Long> startTime = new ThreadLocal<>();
   private final ThreadLocal<Long> startTimeCpuNanos = new ThreadLocal<>();
+  private final ThreadLocal<Boolean> finished = ThreadLocal.withInitial(() -> false);
   private final ConcurrentMap<MetricName, AtomicInteger> gauges = new ConcurrentHashMap<>();
+  private final String PROPERTY_NAME = "io.opentracing.contrib.jaxrs2.internal.SpanWrapper.activeSpanWrapper";
 
   private Tracer tracer;
-  private List<ServerSpanDecorator> spanDecorators;
-  private OperationNameProvider.Builder operationNameProvider;
 
-  public WavefrontJerseyFilter(SdkReporter wfJerseyReporter, ApplicationTags applicationTags) {
+  public WavefrontJerseyFilter(SdkReporter wfJerseyReporter, ApplicationTags applicationTags,
+                               @Nullable Tracer tracer) {
     Preconditions.checkNotNull(wfJerseyReporter, "Invalid JerseyReporter");
     Preconditions.checkNotNull(applicationTags, "Invalid ApplicationTags");
     this.wfJerseyReporter = wfJerseyReporter;
     this.applicationTags = applicationTags;
-  }
-
-  public WavefrontJerseyFilter(SdkReporter wfJerseyReporter, ApplicationTags applicationTags, Tracer tracer) {
-    this(wfJerseyReporter, applicationTags);
     this.tracer = tracer;
-    this.spanDecorators = Collections.singletonList(ServerSpanDecorator.STANDARD_TAGS);
   }
 
   @Override
@@ -98,12 +86,8 @@ public class WavefrontJerseyFilter implements ContainerRequestFilter, ContainerR
           spanBuilder.asChildOf(parentSpanContext);
         }
         Scope scope = spanBuilder.startActive(false);
-        if (spanDecorators != null) {
-          for (ServerSpanDecorator decorator: spanDecorators) {
-            decorator.decorateRequest(containerRequestContext, scope.span());
-          }
-        }
-        containerRequestContext.setProperty(SpanWrapper.PROPERTY_NAME, new SpanWrapper(scope));
+        decorateRequest(containerRequestContext, scope.span());
+        containerRequestContext.setProperty(PROPERTY_NAME, scope);
       }
 
        /* Gauges
@@ -127,17 +111,19 @@ public class WavefrontJerseyFilter implements ContainerRequestFilter, ContainerR
   public void filter(ContainerRequestContext containerRequestContext,
                      ContainerResponseContext containerResponseContext)
       throws IOException {
-    SpanWrapper spanWrapper = CastUtils.cast(containerRequestContext.getProperty(PROPERTY_NAME), SpanWrapper.class);
-    if (spanWrapper == null) {
-      return;
-    }
-    if (spanDecorators != null) {
-      for (ServerSpanDecorator decorator: spanDecorators) {
-        decorator.decorateResponse(containerResponseContext, spanWrapper.get());
+    try {
+      Scope scope = (Scope) containerRequestContext.getProperty(PROPERTY_NAME);
+      if (scope != null) {
+        decorateResponse(containerResponseContext, scope.span());
+        scope.close();
+        if (!finished.get()) {
+          finished.set(true);
+          scope.span().finish();
+        }
       }
+    } catch (ClassCastException ex) {
+      // no valid scope found
     }
-    spanWrapper.getScope().close();
-    spanWrapper.finish();
     if (containerRequestContext instanceof ContainerRequest) {
       ContainerRequest request = (ContainerRequest) containerRequestContext;
       ExtendedUriInfo uriInfo = request.getUriInfo();
@@ -337,86 +323,6 @@ public class WavefrontJerseyFilter implements ContainerRequestFilter, ContainerR
     }
   }
 
-  @Override
-  public void init(FilterConfig filterConfig) {
-  }
-
-  @Override
-  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-      throws IOException, ServletException {
-
-    HttpServletResponse httpResponse = (HttpServletResponse)response;
-    HttpServletRequest httpRequest = (HttpServletRequest)request;
-
-    try {
-      chain.doFilter(request, response);
-    } catch (Exception ex) {
-      SpanWrapper spanWrapper = getSpanWrapper(httpRequest);
-      if (spanWrapper != null) {
-        Tags.HTTP_STATUS.set(spanWrapper.get(), httpResponse.getStatus());
-        // addExceptionLogs(spanWrapper.get(), ex);
-      }
-      throw ex;
-    } finally {
-      SpanWrapper spanWrapper = getSpanWrapper(httpRequest);
-      if (spanWrapper != null) {
-        spanWrapper.getScope().close();
-        if (request.isAsyncStarted()) {
-          request.getAsyncContext().addListener(new SpanFinisher(spanWrapper), request, response);
-        } else {
-          spanWrapper.finish();
-        }
-      }
-    }
-  }
-
-  @Override
-  public void destroy() {
-
-  }
-
-  private SpanWrapper getSpanWrapper(HttpServletRequest request) {
-    return CastUtils.cast(request.getAttribute(SpanWrapper.PROPERTY_NAME), SpanWrapper.class);
-  }
-
-  static class SpanFinisher implements AsyncListener {
-    private SpanWrapper spanWrapper;
-    SpanFinisher(SpanWrapper spanWrapper) {
-      this.spanWrapper = spanWrapper;
-    }
-
-    @Override
-    public void onComplete(AsyncEvent event) throws IOException {
-      HttpServletResponse httpResponse = (HttpServletResponse)event.getSuppliedResponse();
-      if (httpResponse.getStatus() >= 500) {
-        addExceptionLogs(spanWrapper.get(), event.getThrowable());
-      }
-      Tags.HTTP_STATUS.set(spanWrapper.get(), httpResponse.getStatus());
-      spanWrapper.finish();
-    }
-    @Override
-    public void onTimeout(AsyncEvent event) throws IOException {
-    }
-    @Override
-    public void onError(AsyncEvent event) throws IOException {
-      // this handler is called when exception is thrown in async handler
-      // note that exception logs are added in filter not here
-    }
-    @Override
-    public void onStartAsync(AsyncEvent event) throws IOException {
-    }
-  }
-
-  private static void addExceptionLogs(Span span, Throwable throwable) {
-    Tags.ERROR.set(span, true);
-    if (throwable != null) {
-      Map<String, Object> errorLogs = new HashMap<>(2);
-      errorLogs.put("event", Tags.ERROR.getKey());
-      errorLogs.put("error.object", throwable);
-      span.log(errorLogs);
-    }
-  }
-
   private Pair<String, String> getClassAndMethodName(ExtendedUriInfo uriInfo) {
     String className = "unknown";
     String methodName = "unknown";
@@ -465,24 +371,72 @@ public class WavefrontJerseyFilter implements ContainerRequestFilter, ContainerR
     }
   }
 
-  static class CustomeOperationNameProvider implements OperationNameProvider {
-    private final String methodName;
-
-    private CustomeOperationNameProvider(String methodName) {
-      this.methodName = methodName;
+  private void decorateRequest(ContainerRequestContext requestContext, Span span) {
+    Tags.COMPONENT.set(span, "jaxrs");
+    Tags.HTTP_METHOD.set(span, requestContext.getMethod());
+    String urlStr = null;
+    URL url;
+    try {
+      url = requestContext.getUriInfo().getRequestUri().toURL();
+      urlStr = url.toString();
+    } catch (MalformedURLException e) {
+      // ignoring returning null
     }
+    if (urlStr != null) {
+      Tags.HTTP_URL.set(span, urlStr);
+    }
+  }
 
-    static class Builder implements OperationNameProvider.Builder {
+  private void decorateResponse(ContainerResponseContext responseContext, Span span) {
+    Tags.HTTP_STATUS.set(span, responseContext.getStatus());
+  }
 
-      @Override
-      public OperationNameProvider build(Class<?> clazz, Method method) {
-        return new CustomeOperationNameProvider(method.getName());
-      }
+  public class ServerHeadersExtractTextMap implements TextMap {
+
+    private final MultivaluedMap<String, String> headers;
+
+    ServerHeadersExtractTextMap(MultivaluedMap<String, String> headers) {
+      this.headers = headers;
     }
 
     @Override
-    public String operationName(ContainerRequestContext requestContext) {
-      return methodName;
+    public Iterator<Map.Entry<String, String>> iterator() {
+      return new MultivaluedMapFlatIterator<>(headers.entrySet());
+    }
+
+    @Override
+    public void put(String key, String value) {
+      throw new UnsupportedOperationException(
+              ServerHeadersExtractTextMap.class.getName() +" should only be used with Tracer.extract()");
+    }
+  }
+
+  public static final class MultivaluedMapFlatIterator<K, V> implements Iterator<Map.Entry<K, V>> {
+    private final Iterator<Map.Entry<K, List<V>>> mapIterator;
+    private Map.Entry<K, List<V>> mapEntry;
+    private Iterator listIterator;
+
+    MultivaluedMapFlatIterator(Set<Map.Entry<K, List<V>>> multiValuesEntrySet) {
+      this.mapIterator = multiValuesEntrySet.iterator();
+    }
+
+    public boolean hasNext() {
+      return this.listIterator != null && this.listIterator.hasNext() || this.mapIterator.hasNext();
+    }
+
+    public Map.Entry<K, V> next() {
+      if (this.mapEntry == null || !this.listIterator.hasNext() && this.mapIterator.hasNext()) {
+        this.mapEntry = this.mapIterator.next();
+        this.listIterator = ((List)this.mapEntry.getValue()).iterator();
+      }
+
+      return this.listIterator.hasNext() ?
+              new AbstractMap.SimpleImmutableEntry(this.mapEntry.getKey(), this.listIterator.next()) :
+              new AbstractMap.SimpleImmutableEntry(this.mapEntry.getKey(), (Object)null);
+    }
+
+    public void remove() {
+      throw new UnsupportedOperationException();
     }
   }
 }
